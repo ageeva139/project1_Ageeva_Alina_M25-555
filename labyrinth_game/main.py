2d5d56206a46a1d4342187b0c28eee31d8175337
ageeva139 <ageevaa2002@gmail.com>
2025-11-13 18:01:31 +0300

    Создание файлов constants.py, player_actions.py, utils.py и определение карты комнат

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,3 +1,12 @@
+from labyrinth_game import constants
+
+game_state = {
+    'player_inventory': [], # Инвентарь игрока
+    'current_room': 'entrance', # Текущая комната
+    'game_over': False, # Значения окончания игры
+    'steps_taken': 0 # Количество шагов
+}
+
 def main():
     print("Первая попытка запустить проект!")
 
